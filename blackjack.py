e8d8d778e46e6ab21cfb525af1efe8d875b675be
Bavarcarus <82047589+Bavarcarus@users.noreply.github.com>
2021-04-25 18:43:45 -0400

    Added screen clearing, print formatting

--- blackjack.py
+++ blackjack.py
@@ -1,40 +1,57 @@
 #This is my blackjack python program
 import random
+from os import system, name
+def clear():
+    # for windows
+    if name == 'nt':
+        _ = system('cls')
+  
+    # for mac and linux(here, os.name is 'posix')
+    else:
+        _ = system('clear')
+blackjack_title = '''
+  ____  _        _    ____ _  __   _   _    ____ _  __
+ | __ )| |      / \  / ___| |/ /  | | / \  / ___| |/ /
+ |  _ \| |     / _ \| |   | ' /_  | |/ _ \| |   | ' / 
+ | |_) | |___ / ___ \ |___| . \ |_| / ___ \ |___| . \ 
+ |____/|_____/_/   \_\____|_|\_\___/_/   \_\____|_|\_\                                                      
+'''
 suit = ['Hearts', 'Diamonds', 'Spades', 'Clubs']
 card_value = {'Two': 2, 'Three': 3, 'Four': 4, 'Five': 5, 'Six': 6, 'Seven': 7, 'Eight': 8, 'Nine': 9, 'Ten': 10, 'Jack': 10, 'Queen': 10, 'King': 10, 'Ace': 1}
 cards = ['Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace']
 
 class Hand:
 
-    def __init__(self):
+    def __init__(self, name):
+        self.name = name
         self.player_cards = [[random.choice(cards), random.choice(suit)], [random.choice(cards), random.choice(suit)]]
-        self.score = 0
-        self.scoring()
+        self.scoring()       
 
     def __repr__(self):
-        blackjack_title = '''
-  ____  _        _    ____ _  __   _   _    ____ _  __
- | __ )| |      / \  / ___| |/ /  | | / \  / ___| |/ /
- |  _ \| |     / _ \| |   | ' /_  | |/ _ \| |   | ' / 
- | |_) | |___ / ___ \ |___| . \ |_| / ___ \ |___| . \ 
- |____/|_____/_/   \_\____|_|\_\___/_/   \_\____|_|\_\                                                      
-        '''
-        return blackjack_title
+        return f"{self.name}'s Blackjack game"
 
     def hit_me(self):
+        self.hand_print()
         self.stand = False
         while self.score < 21 and self.stand == False:
             self.hit = input("Hit or Stand?").lower()
-            #print(self.hit[0])
             if self.hit[0] == "h":
                 self.player_cards.append([random.choice(cards), random.choice(suit)])
                 self.scoring()
-                print(self.player_cards, self.score)
+                self.hand_print()
             elif self.hit[0] == "s":
                 self.stand = True
-                break
-            else:
-                continue
+        
+
+    def hand_print(self):
+        self.printable_hand = f"{self.name}'s hand: \n"
+        for show_cards in self.player_cards:
+            self.printable_hand += f"{show_cards[0]} of {show_cards[1]}, "
+        self.printable_hand = self.printable_hand[0:-2]
+        clear()
+        print(blackjack_title)
+        print(f"Dealer shows {dealer.player_cards[0][0]} of {dealer.player_cards[0][1]} \n")
+        print(self.printable_hand, self.score)
     
     def scoring(self):
         self.score = 0
@@ -44,9 +61,10 @@ class Hand:
             if 'Ace' in ace and self.score <= 11:
                 self.score += 10
         
-player_1 = Hand()
-dealer = Hand()
-print(repr(player_1))
-print(player_1.player_cards, player_1.score)
-print(dealer.player_cards, dealer.score)
+clear()
+print(blackjack_title)
+player_1 = Hand(input("What should I call you?"))
+dealer = Hand("Dealer")
+#print(repr(player_1))
+
 player_1.hit_me()
