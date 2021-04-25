491cedbc3c1b2ad9cb3c14922d418d71106b1507
Bavarcarus <82047589+Bavarcarus@users.noreply.github.com>
2021-04-25 14:56:37 -0400

    Added scoring, staged hit me

--- blackjack.py
+++ blackjack.py
@@ -5,10 +5,34 @@ card_value = {'Two': 2, 'Three': 3, 'Four': 4, 'Five': 5, 'Six': 6, 'Seven': 7, 
 cards = ['Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace']
 
 class Hand:
+
     def __init__(self):
         self.player_cards = [[random.choice(cards), random.choice(suit)], [random.choice(cards), random.choice(suit)]]
+        self.score = 0
+        self.scoring()
 
+    def __repr__(self):
+        blackjack_title = '''
+  ____  _        _    ____ _  __   _   _    ____ _  __
+ | __ )| |      / \  / ___| |/ /  | | / \  / ___| |/ /
+ |  _ \| |     / _ \| |   | ' /_  | |/ _ \| |   | ' / 
+ | |_) | |___ / ___ \ |___| . \ |_| / ___ \ |___| . \ 
+ |____/|_____/_/   \_\____|_|\_\___/_/   \_\____|_|\_\                                                      
+        '''
+        return blackjack_title
+
+    def hit_me(self):
+        pass
+    
+    def scoring(self):
+        for card in self.player_cards:
+            self.score += card_value[card[0]]
+        for ace in self.player_cards:
+            if 'Ace' in ace and self.score <= 11:
+                self.score += 10
+        
 player_1 = Hand()
 dealer = Hand()
-print(player_1.player_cards)
-print(dealer.player_cards)
+print(repr(player_1))
+print(player_1.player_cards, player_1.score)
+print(dealer.player_cards, dealer.score)
